4d8ca9cf60ebf329e89afff5b1881daa61617e4a
Remigiusz Wozniak <remek778790@gmail.com>
2022-05-16 22:27:57 +0200

    gh-pages added

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import ContextProvider from "./context/ContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <ContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ContextProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ContextProvider>
+      <BrowserRouter basename="/dream-shop">
+        <App />
+      </BrowserRouter>
+    </ContextProvider>
+  </React.StrictMode>
 );
